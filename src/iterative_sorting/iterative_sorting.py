f51a015decbb2638962bb2f9cb137dff0e1274a8
aapetsi <apetsiampiah@gmail.com>
2020-02-15 17:35:15 +0000

    start work on iterative sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,36 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    is_sorted = sorted(arr)
+# TO-DO: Complete the selection_sort() function below
+import random
+
+
+def check_sorted(arr):
+    for index, num in enumerate(arr):
+        if index != len(arr) - 1:
+            if arr[index] > arr[(index + 1)]:
+                return False
+    return True
+
+
+def selection_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    is_sorted = check_sorted(arr)
     ret = []
     while len(arr) != 0:
         for idx, val in enumerate(arr):
             min_val = min(arr[0:])
             min_index = arr.index(min_val)
             ret.append(min_val)
-            print(ret)
-            print(arr)
             arr.pop(min_index)
-            print(min_val, min_index)
-            print(len(ret))
-        is_sorted = sorted(arr)
+        is_sorted = check_sorted(arr)
     return ret
 
-
-
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
 
-    is_sorted = sorted(arr)
+
+def bubble_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    is_sorted = check_sorted(arr)
     while not is_sorted:
         for index, num in enumerate(arr):
             if index != len(arr) - 1:
@@ -29,17 +39,15 @@ def bubble_sort( arr ):
                 if first > second:
                     arr[index] = second
                     arr[index + 1] = first
-        is_sorted = sorted(arr)
+        is_sorted = check_sorted(arr)
     return arr
 
-def sorted(arr):
-    for index, num in enumerate(arr):
-        if index != len(arr) - 1:
-            if arr[index] > arr[(index + 1)]:
-                return False
-    return True
+
+
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+
+
+def count_sort(arr, maximum=-1):
 
     return arr
